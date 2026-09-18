09bfa8773e5ecb045dba36e34564e6cb2b190e84
atarik23 <139481511+atarik23@users.noreply.github.com>
2026-09-18 11:59:34 -0700

    Connect allocation view to portfolio results

--- Implementation/src/ui/MainView.h
+++ Implementation/src/ui/MainView.h
@@ -13,6 +13,7 @@ class MainView : public gui::StandardTabView
     DataView _dataView;
     OptimizationView _optimizationView;
     EfficientFrontierCanvas _frontierView;
+    PortfolioAllocationCanvas _allocationView;
     PortfolioWeightsCanvas _weightsView;
 
 public:
@@ -20,24 +21,28 @@ public:
         : _dataView(_state)
         , _optimizationView(_state)
         , _frontierView(_state)
+        , _allocationView(_state)
         , _weightsView(_state)
     {
         _dataView.setOnDataLoaded([this]()
         {
             _optimizationView.onDataLoaded();
             _frontierView.refresh();
+            _allocationView.refresh();
             _weightsView.refresh();
         });
 
         _optimizationView.setOnResultsChanged([this]()
         {
             _frontierView.refresh();
+            _allocationView.refresh();
             _weightsView.refresh();
         });
 
         addView(&_dataView, tr("dataTab"));
         addView(&_optimizationView, tr("optimizationTab"));
         addView(&_frontierView, tr("frontierTab"));
+        addView(&_allocationView, tr("allocationTab"));
         addView(&_weightsView, tr("weightsTab"));
         setCurrentViewPos(0);
     }
